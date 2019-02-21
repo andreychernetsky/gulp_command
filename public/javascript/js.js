f2b9bd4c8f70cf7306d9f275fc80a76811f8ff76
andric <you@example.com>
2019-02-21 19:21:19 +0300

    new header for site

---
+++ public/javascript/js.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  // Hidden menu
+  $('.btn-menu, .hidden-menu ul a').click(function () {
+    if ($('.hidden-menu').is(':hidden')) {
+      $('.hidden-menu').show();
+    } else {
+      $('.hidden-menu').hide();
+    }
+  });
+
+
+});
