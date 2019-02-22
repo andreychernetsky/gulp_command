b84528fe04072dc1d55efe6ab18a1639c935f0bd
andric <you@example.com>
2019-02-22 19:53:53 +0300

    burger-mobule

---
+++ public/javascript/js.js
@@ -0,0 +1,12 @@
+
+
+
+let  btn = document.querySelector('.section');
+let  nav = document.getElementById('nav');
+
+btn.addEventListener('click', function() {
+  nav.classList.toggle('active');
+});
+
+
+
